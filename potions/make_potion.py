f6c96aa8939f93d0b10587ba27a0df6e69107fc5
ASPP Student <student@localhost>
2019-09-04 10:16:59 +0200

    best potion ever

--- potions/make_potion.py
+++ potions/make_potion.py
@@ -4,6 +4,7 @@ import potions.tools.ingredients
 import potions.tools.cooking
 import potions.tools.inspection
 
+from potions.tools import ingredients, cooking
 
 def make_example_potion(name):
 
@@ -17,14 +18,16 @@ def make_example_potion(name):
 
 
 def make_python_expert_potion(name):
-
-    pass
-
+    my_potion = potions.potion.Potion(name=name)
+    my_potion.setup(container=potions.tools.equipment.pewter_cauldron, heat_source=potions.tools.equipment.fire)
+    my_potion.add_ingredients(ingredients = [ingredients.fish_eyes, ingredients.unicorn_hair, ingredients.tea_leaves])
+    cooking.stir(my_potion, 'anti-clockwise')
+    potions.tools.cooking.simmer(my_potion, duration=2)
+    return my_potion
 
 if __name__ == '__main__':
 
     my_name = 'Potter'
-    my_potion = make_example_potion(name=my_name)
+    my_potion = make_python_expert_potion(my_name)
     # Let Snape inspect the potion
-    potions.tools.inspection.inspection_by_Snape(potion=my_potion, target_potion='example_potion')
-
+    potions.tools.inspection.inspection_by_Snape(potion=my_potion, target_potion='python_expert')
