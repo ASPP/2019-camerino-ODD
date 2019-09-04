9494c2bba6c7ccd363a8d7bddeb27ac569da0d5f
ASPP Student <student@localhost>
2019-09-04 10:04:32 +0200

    make python expert potion
    Fixes: #3

--- potions/make_potion.py
+++ potions/make_potion.py
@@ -18,13 +18,27 @@ def make_example_potion(name):
 
 def make_python_expert_potion(name):
 
-    pass
+    my_potion = potions.potion.Potion(name=name)
+    # Set up your pewter cauldron and light a fire underneath it.
+    my_potion.setup(container=potions.tools.equipment.pewter_cauldron, heat_source=potions.tools.equipment.fire)
+    # Add fish eyes, unicorn hair and tea leaves 
+    my_potion.add_ingredients(ingredients=[
+                              potions.tools.ingredients.fish_eyes,
+                              potions.tools.ingredients.unicorn_hair,
+                              potions.tools.ingredients.tea_leaves])
+    # Stir anti-clockwise
+    potions.tools.cooking.stir(my_potion, direction='anti-clockwise')
+    # Simmer for 2 hours.
+    potions.tools.cooking.simmer(my_potion, duration=2)
+
+    return my_potion
+
 
 
 if __name__ == '__main__':
 
     my_name = 'Potter'
-    my_potion = make_example_potion(name=my_name)
+    my_potion = make_python_expert_potion(name=my_name)
     # Let Snape inspect the potion
-    potions.tools.inspection.inspection_by_Snape(potion=my_potion, target_potion='example_potion')
+    potions.tools.inspection.inspection_by_Snape(potion=my_potion, target_potion='python_expert')
 
